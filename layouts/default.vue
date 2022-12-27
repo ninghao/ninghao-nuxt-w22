2eda36d0898138b776b97ede181cd768bcfbde36
wanghao8080 <wanghao@ninghao.net>
2022-12-27 10:26:16 +0800

    Nuxt 页面的布局：Layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <header>
+      <div><span>🌲</span> {{ name }}</div>
+      <nav></nav>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { name } = useAppConfig();
+</script>
+
+<style>
+@import '~/assets/styles/default.css';
+</style>
