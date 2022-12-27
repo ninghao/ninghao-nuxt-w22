f5de4b3f9e9ce231dbdd614f8f27d2106c789433
wanghao8080 <wanghao@ninghao.net>
2022-12-27 13:16:33 +0800

    在 Nuxt 路由中间件里做重定向

---
+++ pages/login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>用户登录</h1>
+  </div>
+</template>
--- pages/posts/[id]/edit.vue
+++ pages/posts/[id]/edit.vue
@@ -8,4 +8,8 @@
 const {
   params: { id },
 } = useRoute();
+
+definePageMeta({
+  middleware: ['auth'],
+});
 </script>
