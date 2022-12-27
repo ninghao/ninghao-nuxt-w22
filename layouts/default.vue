e0c8a4978940e9aae47f6c9ecf455e79c9e82c91
wanghao8080 <wanghao@ninghao.net>
2022-12-27 11:51:44 +0800

    Nuxt 应用里的路由链接：NuxtLink

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="app">
     <header>
-      <div><span>🌲</span> {{ name }}</div>
-      <nav></nav>
+      <div>
+        <span>🌲</span> <NuxtLink to="/">{{ name }}</NuxtLink>
+      </div>
+      <nav>
+        <div>
+          <NuxtLink to="/about">关于</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink to="/posts">内容</NuxtLink>
+        </div>
+      </nav>
     </header>
     <main>
       <slot></slot>
