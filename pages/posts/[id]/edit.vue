22f3ad2f50ad2b5297e94983b3011e33864b74a9
wanghao8080 <wanghao@ninghao.net>
2022-12-27 11:34:36 +0800

    在 Nuxt 应用里定义带参数的路由

---
+++ pages/posts/[id]/edit.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>内容编辑 {{ id }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {
+  params: { id },
+} = useRoute();
+</script>
---
+++ pages/posts/[id]/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>内容页面 {{ id }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {
+  params: { id },
+} = useRoute();
+</script>
