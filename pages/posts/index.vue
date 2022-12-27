256574519f558030caceca0ffb380eb8355505c1
wanghao8080 <wanghao@ninghao.net>
2022-12-27 14:38:49 +0800

    在 Nuxt 应用里使用异步数据（useAsyncData）

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
     <h1>内容列表</h1>
-    <div v-for="post in posts" :key="post.id">
-      <div>{{ post.content }}</div>
-      <div>
-        - <small>{{ post.user.name }}</small>
+    <div v-if="pending">加载中...</div>
+    <div v-if="posts !== null">
+      <div v-for="post in posts" :key="post.id">
+        <div>{{ post.content }}</div>
+        <div>
+          - <small>{{ post.user.name }}</small>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-const posts = await $fetch('https://nid-node.ninghao.co/posts');
-console.log(posts);
+const {
+  data: posts,
+  pending,
+  refresh,
+  error,
+} = await useAsyncData(
+  'posts',
+  () => $fetch('https://nid-node.ninghao.co/posts'),
+  { lazy: true },
+);
 </script>
