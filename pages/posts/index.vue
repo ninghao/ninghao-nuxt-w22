6f02f1993c2179849e1b78061526eb24712df561
wanghao8080 <wanghao@ninghao.net>
2022-12-27 14:32:09 +0800

    在 Nuxt 应用里使用 $fetch 发送网络请求

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
     <h1>内容列表</h1>
+    <div v-for="post in posts" :key="post.id">
+      <div>{{ post.content }}</div>
+      <div>
+        - <small>{{ post.user.name }}</small>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+const posts = await $fetch('https://nid-node.ninghao.co/posts');
+console.log(posts);
+</script>
