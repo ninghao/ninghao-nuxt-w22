b0dbf2fc526a995843f2ef9d0a371372837d4e2a
wanghao8080 <wanghao@ninghao.net>
2022-12-27 10:00:29 +0800

    在 Nuxt 组件里自定义网页标题（Head、Title 与 useHead）

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
   <NuxtPage />
 </template>
+
+<script setup lang="ts">
+useHead({
+  titleTemplate(title) {
+    return title ? `${title} - 宁皓网` : '宁皓网';
+  },
+});
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
+    <!-- <Head>
+      <Title>关于</Title>
+    </Head> -->
     <h1>关于</h1>
     <div>宁皓网创建于 2011 年。</div>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: '关于',
+});
+</script>
