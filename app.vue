a2dbd247a6bebbbff4a01f05c71bc7d9648250a7
wanghao8080 <wanghao@ninghao.net>
2022-12-27 09:29:52 +0800

    Nuxt 框架的自动导入功能

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <AppHome />
   </div>
 </template>
---
+++ components/app/app-home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>首页</h1>
+    <div>我在宁皓网学习 Nuxt.js。</div>
+  </div>
+</template>
