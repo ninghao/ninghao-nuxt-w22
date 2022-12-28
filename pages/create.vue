52c3ab2caafae5242de3d70aae6e8a14e6949ad2
wanghao8080 <wanghao@ninghao.net>
2022-12-28 10:35:28 +0800

    在 Nuxt 应用里实现上传文件（准备）

--- pages/create.vue
+++ pages/create.vue
@@ -1,6 +1,21 @@
 <template>
   <form @submit.prevent>
     <div>
+      <button
+        class="bordered"
+        @click="($refs.fileInput as HTMLInputElement).click()"
+      >
+        选择图像文件
+      </button>
+      <input
+        type="file"
+        hidden
+        accept="image/png, image/jpeg, image/jpg"
+        ref="fileInput"
+        @change="onChangeFile"
+      />
+    </div>
+    <div>
       <input type="text" placeholder="标题" v-model="title" />
     </div>
     <div>
@@ -17,6 +32,17 @@ useHead({ title: '创建内容' });
 
 const title = ref('');
 const content = ref('');
+const file = ref<File>();
+
+const onChangeFile = (event: Event) => {
+  const { files } = event.target as HTMLInputElement;
+
+  if (files) {
+    file.value = files[0];
+  }
+
+  console.log(file.value);
+};
 
 const createPost = async () => {
   const { data } = await useApiFetch('posts', {
